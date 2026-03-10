3210cf95637de24661fe8a57debcb85d9751cd9c
usbport11 <poisk.ctulhu@gmail.com>
2026-03-10 20:56:49 +0300

    10032026

--- dnd/forms.py
+++ dnd/forms.py
@@ -1,28 +1,23 @@
 from django import forms
 from .models import Gender, Race, Specialization, State, Scenario, Hero
 
+class_input = """
+    w-full rounded-sm border
+    border-neutral-300 bg-neutral-50 px-2 py-2 text-sm focus-visible:outline-2
+    focus-visible:outline-offset-2 focus-visible:outline-black disabled:cursor-not-allowed
+    disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white
+"""
+
 class ScenarioForm(forms.ModelForm):
     class Meta:
         model = Scenario
         fields = ['name', 'description']
-        class_input = """
-          w-full rounded-sm border 
-	  border-neutral-300 bg-neutral-50 px-2 py-2 text-sm focus-visible:outline-2 
-	  focus-visible:outline-offset-2 focus-visible:outline-black disabled:cursor-not-allowed 
-	  disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white
-        """
         widgets = {
             'name': forms.TextInput(attrs={'class':class_input}),
             'description': forms.TextInput(attrs={'class':class_input})
         }
 
 class HeroForm(forms.ModelForm):
-    class_input = """
-        w-full rounded-sm border
-        border-neutral-300 bg-neutral-50 px-2 py-2 text-sm focus-visible:outline-2
-        focus-visible:outline-offset-2 focus-visible:outline-black disabled:cursor-not-allowed
-        disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white
-    """
     gender = forms.ModelChoiceField(queryset=Gender.objects.all(),
       widget=forms.Select(attrs={'class': class_input}))
     race = forms.ModelChoiceField(queryset=Race.objects.all(),
@@ -36,12 +31,6 @@ class HeroForm(forms.ModelForm):
     class Meta:
         model = Hero
         fields = ["name", "description", "history", "age", "gender", "race", "specialization", "health", "mana", "state", "scenario"]
-        class_input = """
-          w-full rounded-sm border
-          border-neutral-300 bg-neutral-50 px-2 py-2 text-sm focus-visible:outline-2
-          focus-visible:outline-offset-2 focus-visible:outline-black disabled:cursor-not-allowed
-          disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white
-        """
         widgets = {
             'name': forms.TextInput(attrs={'class':class_input}),
             'description': forms.TextInput(attrs={'class':class_input}),
